dd7edba5a04c0584834caeed33e596bce0de9efb
Shento Hendriks <70943155+ShentoHendriks@users.noreply.github.com>
2026-08-22 20:53:44 +0200

    Install v1.0.64

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-VERSION=v1.0.63
+VERSION=v1.0.64
 ARCHIVE_URL="https://github.com/ShentoHendriks/rwa-vps-installer/releases/download/${VERSION}/rwa-vps-panel-${VERSION}.tar.gz"
 WORK_DIR="$(mktemp -d)"
 trap 'rm -rf "$WORK_DIR"' EXIT
